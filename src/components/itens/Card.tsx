5a90d90e64ffc95323c4086508cacbae1a26af03
Augusto_Carrara <augusto71magalhaes@gmail.com>
2023-11-28 20:48:46 -0300

    ultimas alterações nos cards

--- src/components/itens/Card.tsx
+++ src/components/itens/Card.tsx
@@ -16,8 +16,12 @@ function Card ({produto}: {produto: Produto}){
                 <div className={`card-body ${styles.card_body}`}>
                     <h2 className={`card-title ${styles.card_title}`}>{produto.name}</h2>
                     <p className={`card-text ${styles.card_text}`}>{produto.descricao}</p>
-                    <h4>Valor: {produto.price}</h4>
-
+                    <div className="price-button-container">
+                        <div className="d-flex align-items-center">
+                            <h4 className="mb-0 me-2">Valor: {produto.price}</h4>
+                            <button className={`btn btn-success ${styles.buyButton}`}>Comprar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
